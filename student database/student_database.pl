99847f213ed9190d5e4cb522b822c286d1c58129
Mateusz Sobolewski <47489401+matekstatek@users.noreply.github.com>
2022-06-08 10:29:01 +0200

    Update student_database.pl

--- student database/student_database.pl
+++ student database/student_database.pl
@@ -1,22 +1,23 @@
-assert(student(18342, mateusz, sobolewski, 4.8, informatyka)).
-assert(student(18433, katarzyna, garus, 4.0, nawigacja)).
-assert(student(18556, katarzyna, losicka, 4.1, mechatronika)).
-assert(student(18432, pawel, kubacki, 3.8, informatyka)).
-assert(student(17654, maksymilian, wrzesien, 4.5, mechanika)).
-assert(student(24562, arkadiusz, kot, 3.5, informatyka)).
-assert(student(16647, paulina, skwarzec, 4.0, nawigacja)).
-assert(student(25385, eryk, perzewski, 4.1, mechatronika)).
-assert(student(12387, dawid, pianka, 4.2, mechanika)).
-assert(student(65839, szymon, pianka, 4.3, mechatronika)).
-assert(student(12754, przemek, pianka, 3.1, nawigacja)).
-assert(student(26458, marcin, przybysz, 4.3, informatyka)).
-assert(student(11111,arkadiusz,kopec,4.1,mechatronika)).
-assert(student(18943,damian,peza,3.9,mechanika)).
-retract(student(11111,arkadiusz,kopec,4.1,mechatronika)).
-assert(student(11111,mateusz,stateusz,3.4,informatyka)).
-assert(student(12345,matek,stateczny,4.6,mechatronika)).
-retract(student(12345,matek,stateczny,4.6,mechatronika)).
-assert(student(12345,matek,stateczny,4.5,informatyka)).
-retract(student(12345,matek,stateczny,4.5,informatyka)).
-retract(student(11111,mateusz,stateusz,3.4,informatyka)).
-assert(student(18888,matek,statek,4.5,info)).
+assert(student(18342, james, payne, 4.8, it)).
+assert(student(18433, michael, baker, 4.0, navigation)).
+assert(student(18556, robert, walker, 4.1, mechatronics)).
+assert(student(18432, anthony, lewis, 3.8, it)).
+assert(student(17654, andrew, stevens, 4.5, mechanic)).
+assert(student(24562, roy, harris, 3.5, it)).
+assert(student(16647, anthony, burton, 4.0, navigation)).
+assert(student(25385, robert, wilson, 4.1, mechatronics)).
+assert(student(12387, james, adams, 4.2, mechanic)).
+assert(student(65839, andrew, adamson, 4.3, mechatronics)).
+assert(student(12754, bobby, adams, 3.1, navigation)).
+assert(student(26458, anthony, owen, 4.3, it)).
+assert(student(11111,robert,holmes,4.1,mechatronics)).
+assert(student(18943,bobby,chapman,3.9,mechanic)).
+retract(student(11111,roy,webb,4.1,mechatronics)).
+assert(student(11111,randy,allen,3.4,it)).
+assert(student(12345,robert,jones,4.6,mechatronics)).
+retract(student(12345,michael,lewis,4.6,mechatronics)).
+assert(student(12345,james,foster,4.5,it)).
+retract(student(12345,bobby,foster,4.5,it)).
+retract(student(11111,michael,white,3.4,it)).
+assert(student(18888,bobby,wilson,4.5,it)).
+assert(student(12331,anthony,hopkins,5.0,hr)).
